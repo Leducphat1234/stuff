d52f3eb979f1c260788493f1e39e087685d18512
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2022-10-05 21:46:46 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -18,22 +18,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Listening on port {}".format(port))
     s.listen()
     conn, addr = s.accept()
-    print("Connected to address {}".format(addr))
+    print("\aConnected to address {}".format(addr))
     with conn:
-        conn.settimeout(1)
         try:
             while True:
                 command = input("Command: ")
                 while command == "":
                     command = input("Command: ")
+                
                 conn.send(command.encode())
-                try:
-                    returns = conn.recv(2048).decode('utf-8')
-                    print(returns)
+                
+                returns = conn.recv(2048).decode('utf-8')
+                print(returns)
 
-                    exceptions = conn.recv(2048).decode('utf-8')
-                    print(exceptions)
-                except TimeoutError:
-                    pass
+                exceptions = conn.recv(2048).decode('utf-8')
+                print(exceptions)
+                if "sys.exit" in command:
+                    break
         except Exception as e:
             print("Socket Error:", e)
